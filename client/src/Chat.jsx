2d11dcd88c69ea0a98a2881e1a69567b95384d0d
aakritinayak <nayakaakriti@gmail.com>
2024-12-18 19:48:33 +0530

    modified

---
+++ client/src/Chat.jsx
@@ -0,0 +1,20 @@
+import react from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache(),
+  });
+
+  const Chat = () => {
+    return (
+        <div>chat window
+
+        </div>
+    )
+}
+export default () => (
+    <ApolloProvider client ={client}>
+        <chat />
+        
+    </ApolloProvider>
+)
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { Alert } from "shards-react";
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "shards-ui/dist/css/shards.min.css"
+import "shards-ui/dist/css/shards.min.css";
 
 
-createRoot(document.getElementById('root')).render(
+import Chat from "./Chat";
+
+const ChatApp = () => <Chat />;
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <ChatApp /> {/* Rendered the ChatApp component */}
+  </StrictMode>
+);
